321c9e8c3d6c51febfcbcfb10b14e9739bcc35e4
None <None>
2024-07-17 20:49:20 +0900

    fix

--- bigquery_timeseries/sql/resample.py
+++ bigquery_timeseries/sql/resample.py
@@ -1,11 +1,12 @@
 # resample.py
 
 from dataclasses import dataclass
-from typing import List, Optional, Any, Dict, Union
+from typing import List, Optional, Any
 import pandas as pd
 from google.cloud import bigquery
 from .basic import to_where
-
+from loguru import logger
+from .basic import Query
 
 @dataclass(frozen=True)
 class Expr:
@@ -76,7 +77,7 @@ class Like(Expr):
 
     def to_repr(self) -> str:
         assert isinstance(self.value, str)
-        return f"{self.field} like {self.value}"
+        return f"{self.field} like '{self.value}'"
 
 
 @dataclass(frozen=True)
@@ -86,8 +87,7 @@ class Eq(Expr):
 
     def to_repr(self) -> str:
         if isinstance(self.value, str):
-            assert isinstance(self.value, str)
-            return f"{self.field} = \'{self.value}\'"
+            return f"{self.field} = '{self.value}'"
         if isinstance(self.value, bool):
             str_value = "true" if self.value is True else "false"
             return f"{self.field} = {str_value}"
@@ -96,99 +96,100 @@ class Eq(Expr):
         raise NotImplementedError
 
 
-class ResampleQuery:
+class ResampleQuery(Query):
     def __init__(self, project_id: str, dataset_id: str, bq_client: bigquery.Client):
-        self.project_id = project_id
-        self.dataset_id = dataset_id
-        self.bq_client = bq_client
-
+        super().__init__(project_id, dataset_id, bq_client)
 
     def resample_query(
         self,
         table_name: str,
-        fields: List[str],
-        symbols: Optional[List[str]] = None,
-        start_dt: Optional[str] = None,
-        end_dt: Optional[str] = None,
+        fields: list,
+        start_dt: str,
+        end_dt: str,
+        symbols: list = None,
         interval: str = "day",
-        tz: Optional[str] = None,
-        ops: List[str] = ["last"],
-        where: Optional[Expr] = None,
-        cast: Optional[str] = None,
-        verbose: int = 0,
-        offset_repr: Optional[str] = None,
-        max_cost: float = 1.0
-    ) -> pd.DataFrame:
-        where_clauses = to_where(
-            start_dt=start_dt,
-            end_dt=end_dt,
-            partition_key="partition_dt",
-            partition_interval="monthly",
-            tz=tz
-        )
-        if symbols is not None and len(symbols) > 0:
-            predicated = "\'" + "\',\'".join(symbols) + "\'"
-            where_clauses += [f"symbol in ({predicated})"]
+        ops: list = None,
+        partition_key: str = None,
+        partition_interval: str = None,
+        max_cost: float = 1.0,
+    ):
+        if ops is None:
+            ops = ["first", "last", "min", "max", "mean"]
 
-        table_id = f"{self.project_id}.{self.dataset_id}.{table_name}"
-
-        # リサンプリングのための SQL を構築
-        agg_functions = {
-            "first": "FIRST_VALUE",
-            "last": "LAST_VALUE",
-            "max": "MAX",
-            "min": "MIN",
-            "sum": "SUM",
+        interval_to_sql = {
+            "day": "DATE(dt)",
+            "week": "DATE_TRUNC(dt, WEEK)",
+            "month": "DATE_TRUNC(dt, MONTH)",
+            "quarter": "DATE_TRUNC(dt, QUARTER)",
+            "year": "DATE_TRUNC(dt, YEAR)",
         }
 
-        select_clauses = ["DATE(dt) AS dt"]  # 'dt' カラムを明示的に選択
+        interval_sql = interval_to_sql.get(interval, "DATE(dt)")
+
+        agg_functions = []
         for field in fields:
             for op in ops:
-                if op == 'first':
-                    select_clauses.append(
-                        f"FIRST_VALUE({field}) OVER(PARTITION BY symbol, DATE(dt) ORDER BY dt) AS {field}_{op}")
-                elif op == 'last':
-                    select_clauses.append(
-                        f"LAST_VALUE({field}) OVER(PARTITION BY symbol, DATE(dt) ORDER BY dt ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS {field}_{op}")
-                elif op in ['max', 'min', 'sum']:
-                    select_clauses.append(f"{op.upper()}({field}) AS {field}_{op}")
+                if op == "first":
+                    agg_functions.append(
+                        f"ARRAY_AGG({field} IGNORE NULLS ORDER BY dt ASC LIMIT 1)[OFFSET(0)] AS {field}_{op}"
+                    )
+                elif op == "last":
+                    agg_functions.append(
+                        f"ARRAY_AGG({field} IGNORE NULLS ORDER BY dt DESC LIMIT 1)[OFFSET(0)] AS {field}_{op}"
+                    )
+                elif op == "mean":
+                    agg_functions.append(f"AVG({field}) AS {field}_{op}")
+                else:
+                    agg_functions.append(
+                        f"{op.upper()}({field}) AS {field}_{op}")
 
-        stmt = f"""
+        agg_functions_str = ", ".join(agg_functions)
+
+        query = f"""
         WITH daily_data AS (
-            SELECT 
-                symbol,
-                {', '.join(select_clauses)}
-            FROM {table_id}
-            WHERE {' AND '.join(where_clauses)}
+            SELECT
+                DATE(dt) AS dt, symbol, {agg_functions_str}
+            FROM
+                {self.project_id}.{self.dataset_id}.{table_name}
+            WHERE
+                partition_dt >= CAST('{start_dt}' AS DATE) AND
+                dt >= CAST('{start_dt}' AS DATETIME) AND
+                partition_dt <= CAST('{end_dt}' AS DATE) AND
+                dt <= CAST('{end_dt}' AS DATETIME)
+            {"AND symbol in (" + ', '.join([f"'{s}'" for s in symbols]) + ")" if symbols else ""}
+            GROUP BY
+                DATE(dt), symbol
         )
-        SELECT 
-            symbol,
-            dt,
-            {', '.join([f'{field}_{op}' for field in fields for op in ops])}
-        FROM daily_data
-        GROUP BY symbol, dt
-        ORDER BY symbol, dt
+        SELECT
+            *
+        FROM
+            daily_data
+        ORDER BY
+            dt, symbol
         """
 
-        # コスト見積もり
-        job_config = bigquery.QueryJobConfig(dry_run=True, use_query_cache=False)
-        dry_run_query_job = self.bq_client.query(stmt, job_config=job_config)
-        bytes_processed = dry_run_query_job.total_bytes_processed
-        estimated_cost = bytes_processed * 5 / 1e12  # $5 per TB
+        logger.info(f"Executing query: {query}")
 
-        print(
-            f"This query will process approximately {bytes_processed / (1024 ** 3):.2f} GB of data.")
-        print(f"The estimated cost is ${estimated_cost:.4f}.")
+        job_config = bigquery.QueryJobConfig(
+            dry_run=True, use_query_cache=False)
+        dry_run_query_job = self.bq_client.query(query, job_config=job_config)
+        logger.info(
+            f"This query will process {dry_run_query_job.total_bytes_processed} bytes"
+        )
+
+        estimated_cost = dry_run_query_job.total_bytes_processed * 5 / 1e12
+        logger.info(f"Estimated cost: ${estimated_cost:.6f}")
 
         if estimated_cost > max_cost:
             raise ValueError(
-                f"Estimated cost (${estimated_cost:.4f}) exceeds the maximum allowed cost (${max_cost:.2f}). Query execution cancelled.")
+                f"Estimated query cost (${estimated_cost:.6f}) exceeds the maximum allowed cost (${max_cost:.2f})"
+            )
 
-        query_job = self.bq_client.query(stmt)
-        result = query_job.result()
+        query_job = self.bq_client.query(query)
+        results = query_job.result()
 
-        df = result.to_dataframe()
-        df['dt'] = pd.to_datetime(df['dt'])
-        df = df.set_index('dt').sort_index()
+        df = results.to_dataframe()
+        df["dt"] = pd.to_datetime(df["dt"])
+        df.set_index(["dt"], inplace=True)
 
         return df
