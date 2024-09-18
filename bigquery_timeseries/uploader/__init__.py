6dd6c78b9145978b7be6aadac9a8d432a487b07c
yoshihiro1909 <yoshihiro1909@gmail.com>
2024-09-18 20:49:48 +0900

    fix

--- bigquery_timeseries/uploader/__init__.py
+++ bigquery_timeseries/uploader/__init__.py
@@ -3,13 +3,15 @@ from .gcs_uploader import GCSUploader
 from .bq_uploader import BQUploader
 from .schema_manager import SchemaManager
 
-class Uploader(BaseUploader, GCSUploader, BQUploader):
+class Uploader(BaseUploader):
     def __init__(self, project_id: str, dataset_id: str):
         super().__init__(project_id, dataset_id)
+        self.gcs_uploader = GCSUploader(project_id, dataset_id)
+        self.bq_uploader = BQUploader(project_id, dataset_id)
         self.schema_manager = SchemaManager(self.bq_client)
 
     def upload(self, table_name: str, df, gcs_bucket_name: str, keep_gcs_file: bool = False, max_cost: float = 1.0):
-        gcs_uri = self.upload_to_gcs(gcs_bucket_name, df)
-        self.load_to_bq(table_name, gcs_uri, df, max_cost)
+        gcs_uri = self.gcs_uploader.upload_to_gcs(gcs_bucket_name, df)
+        self.bq_uploader.load_to_bq(table_name, gcs_uri, df, max_cost)
         if not keep_gcs_file:
-            self.delete_gcs_file(gcs_bucket_name, gcs_uri)
+            self.gcs_uploader.delete_gcs_file(gcs_bucket_name, gcs_uri)
